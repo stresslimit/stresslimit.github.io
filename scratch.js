e36beb1eb64eaa241a468db20d7713fe122d3492
Colin Vernon <colin@littlebits.cc>
2015-07-10 14:13:27 -0400

    test use variable for access_token

--- scratch.js
+++ scratch.js
@@ -8,9 +8,9 @@
         return {status: 2, msg: 'Ready'}
     }
 
-    ext.output = function(percent, cb) {
+    ext.output = function(percent, access_token, cb) {
       console.log('sending', percent)
-      var access_token = '8632cdf7594e430744477516d833f3aa0aa645c62477807c44d2f24edd9a2af5'
+      // var access_token = '8632cdf7594e430744477516d833f3aa0aa645c62477807c44d2f24edd9a2af5'
       $.ajax({
         url: 'https://api-http.littlebitscloud.cc/v3/devices/scratch_test_device/output',
         method: 'post',
@@ -25,41 +25,13 @@
       })
     }
 
-    ext.get_temp = function(location, callback) {
-        // Make an AJAX call to the Open Weather Maps API
-        $.ajax({
-              url: 'http://api.openweathermap.org/data/2.5/weather?q='+location+'&units=imperial',
-              dataType: 'jsonp',
-              success: function( weather_data ) {
-                  // Got the data - parse it and return the temperature
-                  temperature = weather_data['main']['temp']
-                  callback(temperature)
-              }
-        })
-    }
-
-    ext.wait_random = function(callback) {
-        wait = Math.random()
-        console.log('Waiting for ' + wait + ' seconds')
-        window.setTimeout(function() {
-            callback()
-        }, wait*1000)
-    }
-
-    ext.power = function(base, exponent) {
-        return Math.pow(base, exponent)
-    }
-
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-          ['w', 'set cloudBit output to %n percent', 'output', 100],
-          ['r', '%n ^ %n', 'power', 2, 3],
-          ['w', 'wait for random time', 'wait_random'],
-          ['R', 'current temperature in city %s', 'get_temp', 'Boston, MA']
+          ['w', 'set cloudBit output to %n percent, with access_token %s', 'output', 100, 'get your access_token from littleBits Cloud Control']
         ]
     }
 
     // Register the extension
-    ScratchExtensions.register('littleBits Test', descriptor, ext)
+    ScratchExtensions.register('littleBits cloudBit', descriptor, ext)
 })({})
