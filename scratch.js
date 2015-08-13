d6c62953ae0981495d5fc421d072a1fe8ad872f0
Colin Vernon <colin@littlebits.cc>
2015-08-12 23:00:26 -0400

    fixing input

--- scratch.js
+++ scratch.js
@@ -4,12 +4,15 @@ https://github.com/byjg/jquery-sse
 !function(n){function t(n){n.type="event",n.instance=new EventSource(n._url),n.instance.successCount=0,n.instance.onmessage=n._settings.onMessage,n.instance.onopen=function(t){0==n.instance.successCount++&&n._settings.onOpen(t)},n.instance.onerror=function(){event.target.readyState==EventSource.CLOSED&&n._settings.onError(event)};for(var t in n._settings.events)n.instance.addEventListener(t,n._settings.events[t],!1)}function e(n){n.type="ajax",n.instance={successCount:0,id:null,retry:3e3,data:"",event:""},s(n)}function s(t){t.instance&&n.ajax({url:t._url,method:"GET",headers:{"Last-Event-ID":t.instance.id},success:function(n,e,i){if(t.instance){0==t.instance.successCount++&&t._settings.onOpen();var a=n.split("\n");t.instance.data="";var c=0;for(var r in a){var o=a[r].split(":");switch(o[0]){case"":o[1]||1!=c++||(eventMessage={data:t.instance.data,lastEventId:t.instance.id,origin:"http://"+i.getResponseHeader("Host"),returnValue:!0},t.instance.event&&t._settings.events[t.instance.event]?t._settings.events[t.instance.event](eventMessage):t._settings.onMessage(eventMessage),t.instance.data="",t.instance.event="",c=0);break;case"retry":c=0,t.instance.retry=parseInt(o[1].trim());break;case"id":c=0,t.instance.id=o[1].trim();break;case"event":c=0,t.instance.event=o[1].trim();break;case"data":c=0,t.instance.data+=(""!=t.instance.data?"\n":"")+o[1].trim();break;default:c=0}}setTimeout(function(){s(t)},t.instance.retry)}},error:t._settings.onError})}n.extend({SSE:function(s,i){var a={instance:null,type:null},c={onOpen:function(){},onEnd:function(){},onError:function(){},onMessage:function(){},options:{},events:{}};return n.extend(c,i),a._url=s,a._settings=c,a._start=a.start,a.start=function(){return this.instance?!1:(!window.EventSource||this._settings.options.forceAjax?e(this):t(this),!0)},a.stop=function(){return this.instance?(!window.EventSource||this._settings.options.forceAjax||this.instance.close(),this._settings.onEnd(),this.instance=null,this.type=null,!0):!1},a}})}(jQuery);
 
 (function(ext) {
-    ext._shutdown = function() {}
+    ext._shutdown = function() {
+      console.log('_shutdown')
+    }
     ext._getStatus = function() {
+      console.log('_getStatus')
       return { status: 2, msg: 'Ready' }
     }
 
-    ext.output = function(cloudBit, percent, access_token, cb) {
+    ext.output = function(cloudBit, percent, duration_ms, access_token, cb) {
       $.ajax({
         url: 'https://api-http.littlebitscloud.cc/v3/devices/'
               + cloudBit
@@ -30,7 +33,7 @@ https://github.com/byjg/jquery-sse
     ext.input = function(cloudBit, access_token, cb) {
       var sse = $.SSE('https://api-http.littlebitscloud.cc/v3/devices/'
                         + cloudBit
-                        + '/output?access_token='
+                        + '/input?access_token='
                         + access_token,
         {
         	onError: function(e) {
@@ -48,8 +51,12 @@ https://github.com/byjg/jquery-sse
 
     var descriptor = {
         blocks: [
-          ['w', 'set cloudBit %s output to %n percent, with access_token %s', 'output', 'ID', 100, 'get your access_token from littleBits Cloud Control'],
-          ['R', 'get cloudBit %s input value, with access_token %s', 'input', 'ID', 'get your access_token from littleBits Cloud Control']
+          ['w', 'set cloudBit %s output value to %n percent for %i ms, with access_token %s',
+            'output',
+            'ID', 100, 3000, 'get your access_token from littleBits Cloud Control'],
+          ['R', 'get cloudBit %s input value, with access_token %s',
+            'input',
+            'ID', 'get your access_token from littleBits Cloud Control']
         ]
     }
 
